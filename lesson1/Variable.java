4ca821cb2a8c0ea27ada2fada3abcb91a6df1078
BanForName <57586598+BanForName@users.noreply.github.com>
2020-10-04 17:04:21 +0500

    first commit

---
+++ lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte processorCore = 8;
+		short processorModel = 6350;
+		int clockFrequency = 3900;
+		long turboFrequency = 4200;
+		float sizeRam = 12.0f;
+		double useSizeRam = 4.8d;
+		char modelRAM = 'H';
+		boolean gameComputer = true;
+
+		System.out.println(processorCore);
+		System.out.println(processorModel);
+		System.out.println(clockFrequency);
+		System.out.println(turboFrequency);
+		System.out.println(sizeRam);
+		System.out.println(useSizeRam);
+		System.out.println(modelRAM);
+		System.out.println(gameComputer);
+	}
+}
