17c25df4064943924da7bdbe92ece429221c4597
BanForName <57586598+BanForName@users.noreply.github.com>
2020-10-05 15:12:44 +0500

    fixed mentor's comments

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,6 +1,5 @@
 public class Cycle {
 	public static void main(String[] args) {
-
 		System.out.println("Вывод всех чисел [0, 20]");
 
 		for(int i = 0; i <= 20; i++) {
@@ -12,21 +11,19 @@ public class Cycle {
 		int num = 6;
 		while(num >= -6) {
 			System.out.println(num);
-			num = num - 2;
+			num -=2;
 		}
 
 		System.out.println("Сумма всех нечетных чисел [10, 20]");
 
-		int i = 10;
-		int sumOddNumbers = 0;
+		num = 10;
+		int sumOdd = 0;
 		do {
-			if (i % 2 != 0) {
-				sumOddNumbers = sumOddNumbers + i;
+			if (num % 2 != 0) {
+				sumOdd +=num;
 			}
-			i++;
-		} while(i <= 20); 
-		System.out.println(sumOddNumbers);
-		
-
+			num++;
+		} while(num <= 20); 
+		System.out.println(sumOdd);
 	}
 }
