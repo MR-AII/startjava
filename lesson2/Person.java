443211b742aa4fd8fd4bcfe806a200dcc152e976
BanForName <57586598+BanForName@users.noreply.github.com>
2021-01-11 11:19:23 +0500

    HW lesson 2

---
+++ lesson2/Person.java
@@ -0,0 +1,22 @@
+Public class Person {
+	String gender = "male";
+	String name = "Alan";
+	float height = 179.3f;
+	float weight = 78.8f;
+
+	void go() {
+	}
+
+	void sitDown() {
+	}
+
+	void run() {
+	}
+
+	void say() {
+	}
+
+	void learnJava() {
+	}
+
+}
