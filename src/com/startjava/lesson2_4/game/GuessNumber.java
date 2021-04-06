631e090f353f2c38f3a7fafe1b12be1267c9e492
BanForName <57586598+BanForName@users.noreply.github.com>
2021-04-06 08:13:48 +0500

    used the ternary operator

--- src/com/startjava/lesson2_4/game/GuessNumber.java
+++ src/com/startjava/lesson2_4/game/GuessNumber.java
@@ -32,24 +32,26 @@ public class GuessNumber {
         showPlayerNums(playerTwo, i);
     }
 
-    public boolean compareNum(Player player, int i) {
-        int num = player.getEnteredNums(i)[i - 1];
-
-        String text1 = player.getName() + " ваше число больше чем загадал компьютер. Осталось попыток: " + player.getTryCount() + "\n";
-        String text2 = player.getName() + " ваше число меньше чем загадал компьютер. Осталось попыток: " + player.getTryCount() + "\n";
-        String text3 = "Игрок: " + player.getName() + " угадал число: " + randomNum + " с " + i + " попытки";
-        System.out.println(num == randomNum ? text3 : num > randomNum ? text1 : text2);
-
-        return num == randomNum;
-    }
-
-    public void inputNum(Player player, int i) {
+    private void inputNum(Player player, int i) {
         System.out.print(player.getName() + " введите число: ");
         player.setEnteredNum(scanner.nextInt(), i - 1);
         player.tryCountDecrement();
     }
 
-    public void showPlayerNums(Player player, int i) {
+    private boolean compareNum(Player player, int i) {
+        int num = player.getEnteredNums(i)[i - 1];
+        if (num == randomNum) {
+            System.out.println("Игрок: " + player.getName() + " угадал число: " + randomNum + " с " + i + " попытки");
+            return true;
+        }
+
+        String result = num > randomNum ? "больше" : "меньше";
+        System.out.println(player.getName() + " ваше число " + result + " чем загадал компьютер. Осталось попыток: " + player.getTryCount() + "\n");
+
+        return false;
+    }
+
+    private void showPlayerNums(Player player, int i) {
         System.out.print("Введенные числа игрока - " + player.getName() + ": ");
         for (int num : player.getEnteredNums(i)) {
             System.out.print(num + " ");
